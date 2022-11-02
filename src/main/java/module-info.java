5d9ab26645f7a22f47895fb36f3658b796ce059e
Elian Vantomme <elian.vantomme@hotmail.be>
2022-11-02 11:26:04 +0100

    changed package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.code {
     requires javafx.fxml;
 
 
-    opens com.example.code to javafx.fxml;
-    exports com.example.code;
+    opens visitor to javafx.fxml;
+    exports visitor;
 }
--- src/main/java/visitor/HelloController.java
+++ src/main/java/visitor/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.code;
+package visitor;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
